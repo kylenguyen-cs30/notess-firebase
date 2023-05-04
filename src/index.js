3fe6eb308767aad14c5b06e473f48acd0f21f585
Kyle Nguyen <knguyen1193@outlook.com>
2023-05-04 10:30:56 -0700

    adding final files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 
 import { initializeApp } from "firebase/app";
 import {
-  getFirestore, collection, addDoc, getDocs
+  getFirestore,
+  collection, 
+  addDoc, 
+  getDocs,
+  onSnapshot
 } from "firebase/firestore";
 import {
   getAuth
@@ -22,6 +26,7 @@ const auth = getAuth(app);
 //welcome message
 const user = auth.currentUser;
 
+const notesRef = collection(db, "users");
 
 auth.onAuthStateChanged(async (user) => {
   if (user) {
@@ -40,37 +45,141 @@ auth.onAuthStateChanged(async (user) => {
   }
 });
 
+window.onload = function () {
+  loadNotes();
+};
 
 
-const notesContainer = document.getElementById("app");
-const addNoteButt = notesContainer.querySelector(".add-note");
-const input = document.getElementById("search_input");
+function loadNotes() {
+  onSnapshot(notesRef, function (snapshot) {
+    // iterate through the notes and display them on the page
+    snapshot.docs.forEach(function (doc) {
+      const note = doc.data();
+      // create a new note element
+      const noteElement = document.createElement("div");
+      noteElement.classList.add("note");
+      const titleElement = document.createElement("h2");
+      titleElement.textContent = note.title;
+      noteElement.appendChild(titleElement);
+      const bodyElement = document.createElement("p");
+      bodyElement.textContent = note.body;
+      noteElement.appendChild(bodyElement);
+
+      // add the note element to the page
+      document.getElementById("app").appendChild(noteElement);
+    });
+  });
+}
+
+function addNote() {
+  // Retrieve the title and body of the note from the input fields
+  const title = document.getElementById("note-title").value;
+  const body = document.getElementById("note-body").value;
+
+  // Create a new note object with the title and body
+  const newNote = {
+    title: title,
+    body: body,
+  };
+
+  // Retrieve a reference to the Firebase database
+  const dbRef = firebase.database().ref();
+
+  // Add the new note to the "notes" collection in the database
+  dbRef.child("notes").push(newNote);
+
+  // Clear the input fields
+  document.getElementById("note-title").value = "";
+  document.getElementById("note-body").value = "";
+}
 
 
-input.addEventListener("input", () => searchFor(input));
-addNoteButt.addEventListener("click", () => addNote());
+
+
+
+
+// function displayNotes(notes) {
+//   notesContainer.innerHTML = "";
+//   Object.values(notes || {}).forEach((note) => {
+//     const noteElement = createNoteElement(note.id, note.content);
+//     notesContainer.appendChild(noteElement);
+//   });
+//   //Display all searched notes then add note button 
+//   notesContainer.appendChild(addNoteButt);
+// }
+
+
+
+// //adding new note function.
+// const notesContainer = document.getElementById("app");
+// const addNoteButt = notesContainer.querySelector(".add-note");
+// addNoteButt.addEventListener("click", () => addNote());
+// function addNote() {
+// 	const existingNotes = getNotes();
+// 	const noteObject = {
+// 		id: Math.floor(Math.random() * 100000), content: ""
+// 	};
+
+// 	const noteElement = createNote(noteObject.id, noteObject.content);
+// 	notesContainer.insertBefore(noteElement, addNoteButt);
+// 	existingNotes.push(noteObject);
+// 	saveNotes(existingNotes);
+// }
+
+
+
+// function createNoteElement(id, content) {
+//   const element = document.createElement("textarea");
+//   element.classList.add("note-text");
+//   element.value = content;
+//   element.placeholder = "Empty note";
+//   element.style.marginBottom = "10px";
+
+//   element.addEventListener("change", () => {
+//     updateNote(id, element.value);
+//   });
+
+//   element.addEventListener("dblclick", () => {
+//     const doDelete = confirm("Are you sure you want to delete?");
+
+//     if (doDelete) {
+//       deleteNote(id, element);
+//     }
+//   });
+//   return element;
+// }
+
+
+
+// function updateNote(id, newContent) {
+//   const noteRef = notesRef.child(id);
+//   noteRef.update({ content: newContent });
+// }
+
+
+// function saveNotes(notes) {
+// 	localStorage.setItem("notess", JSON.stringify(notes));
+// }
+
+
+// const input = document.getElementById("search_input");
+
+
+// input.addEventListener("input", () => searchFor(input));
+
 
 //This is the path to the database
-function searchFor(input) {
-  const searchQuery = input.value.toLowerCase();
-  notesRef.once("value", (snapshot) => {
-    const notes = snapshot.val();
-    const filteredNotes = Object.values(notes || {})
-      .filter((note) => note.content.toLowerCase().includes(searchQuery));
-    displayNotes(filteredNotes);
-  });
-}
+// function searchFor(input) {
+//   const searchQuery = input.value.toLowerCase();
+//   notesRef.once("value", (snapshot) => {
+//     const notes = snapshot.val();
+//     const filteredNotes = Object.values(notes || {})
+//       .filter((note) => note.content.toLowerCase().includes(searchQuery));
+//     displayNotes(filteredNotes);
+//   });
+// }
 
 
-function displayNotes(notes) {
-  notesContainer.innerHTML = "";
-  Object.values(notes || {}).forEach((note) => {
-    const noteElement = createNoteElement(note.id, note.content);
-    notesContainer.appendChild(noteElement);
-  });
-  //Display all searched notes then add note button 
-  notesContainer.appendChild(addNoteButt);
-}
 
 
 //This loads in the notes from Firebase
@@ -80,48 +189,18 @@ function displayNotes(notes) {
 // });
 
 
-function createNoteElement(id, content) {
-  const element = document.createElement("textarea");
-  element.classList.add("note-text");
-  element.value = content;
-  element.placeholder = "Empty note";
-  element.style.marginBottom = "10px";
 
-  element.addEventListener("change", () => {
-    updateNote(id, element.value);
+
+//logout function 
+function logout() {
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+    window.location.href = "login.html"; // redirect the user to the login page
+  }).catch(function (error) {
+    // An error happened.
+    console.log(error);
   });
-
-  element.addEventListener("dblclick", () => {
-    const doDelete = confirm("Are you sure you want to delete?");
-
-    if (doDelete) {
-      deleteNote(id, element);
-    }
-  });
-  return element;
 }
 
 
-function updateNote(id, newContent) {
-  const noteRef = notesRef.child(id);
-  noteRef.update({ content: newContent });
-}
 
-
-function deleteNote(id, element) {
-  const noteRef = notesRef.child(id);
-  noteRef.remove();
-  notesContainer.removeChild(element);
-}
-
-
-function addNote() {
-  const noteObject = {
-    id: Math.floor(Math.random() * 100000),
-    content: ""
-  };
-  const noteRef = notesRef.child(noteObject.id);
-  noteRef.set(noteObject);
-  const noteElement = createNoteElement(noteObject.id, noteObject.content);
-  notesContainer.insertBefore(noteElement, addNoteButt);
-}
